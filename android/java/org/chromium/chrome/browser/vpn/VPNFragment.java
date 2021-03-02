89e7ff405c3f1a622300a1c517d6f4674da629dd
Adrian Hrinko <adriqueh@gmail.com>
2021-03-02 17:14:35 +0100

    VPNFragment added

---
+++ android/java/org/chromium/chrome/browser/vpn/VPNFragment.java
@@ -0,0 +1,268 @@
+package org.chromium.chrome.browser.vpn;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.VpnService;
+import android.os.Bundle;
+import android.os.RemoteException;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import org.chromium.chrome.R;
+
+
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.Fragment;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+import de.blinkt.openvpn.OpenVpnApi;
+import de.blinkt.openvpn.core.OpenVPNService;
+import de.blinkt.openvpn.core.OpenVPNThread;
+import de.blinkt.openvpn.core.VpnStatus;
+
+import static android.app.Activity.RESULT_OK;
+
+public class VPNFragment extends Fragment implements View.OnClickListener { 
+
+    private OpenVPNThread vpnThread = new OpenVPNThread();
+    private OpenVPNService vpnService = new OpenVPNService();
+    boolean vpnStart = false;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_login, container, false);
+
+        return rootView;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Checking is vpn already running or not
+        isServiceRunning();
+        VpnStatus.initLogCache(getActivity().getCacheDir());
+    }
+
+    /**
+     * @param v: click listener view
+     */
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+
+        }
+    }
+
+
+    /**
+     * Prepare for vpn connect with required permission
+     */
+    private void prepareVpn() {
+        if (!vpnStart) {
+            if (getInternetStatus()) {
+
+                // Checking permission for network monitor
+                Intent intent = VpnService.prepare(getContext());
+
+                if (intent != null) {
+                    startActivityForResult(intent, 1);
+                } else startVpn();//have already permission
+
+                // Update confection status
+
+            } else {
+
+                // No internet connection available
+                showToast("you have no internet connection !!");
+            }
+
+        } else if (stopVpn()) {
+
+            // VPN is stopped, show a Toast message.
+            showToast("Disconnect Successfully");
+        }
+    }
+
+    /**
+     * Stop vpn
+     * @return boolean: VPN status
+     */
+    public boolean stopVpn() {
+        try {
+            vpnThread.stop();
+            vpnStart = false;
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+    /**
+     * Taking permission for network access
+     */
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+
+            //Permission granted, start the VPN
+            startVpn();
+        } else {
+            showToast("Permission Deny !! ");
+        }
+    }
+
+    /**
+     * Get service status
+     */
+    public void isServiceRunning() {
+        setStatus(vpnService.getStatus());
+    }
+
+    /**
+     * Start the VPN
+     */
+    private void startVpn() {
+        /*
+        try {
+
+            // .ovpn file
+            InputStream conf = new InputStream();
+            InputStreamReader isr = new InputStreamReader(conf);
+            BufferedReader br = new BufferedReader(isr);
+            String config = "";
+            String line;
+
+            while (true) {
+                line = br.readLine();
+                if (line == null) break;
+                config += line + "\n";
+            }
+
+            br.readLine();
+            OpenVpnApi.startVpn(getContext(), config, "A", "B", "C");
+
+            // Update log
+            binding.logTv.setText("Connecting...");
+            vpnStart = true;
+        } catch (IOException | RemoteException e) {
+            e.printStackTrace();
+        }
+        */
+    }
+
+    /**
+     * Status change with corresponding vpn connection status
+     * @param connectionState
+     */
+    public void setStatus(String connectionState) {
+        if (connectionState!= null)
+        switch (connectionState) {
+            case "DISCONNECTED":
+                break;
+            case "CONNECTED":
+
+                break;
+            case "WAIT":
+                break;
+            case "AUTH":
+                break;
+            case "RECONNECTING":
+                break;
+            case "NONETWORK":
+                break;
+        }
+
+    }
+
+    /**
+     * Receive broadcast message
+     */
+    BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            try {
+                setStatus(intent.getStringExtra("state"));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            try {
+
+                String duration = intent.getStringExtra("duration");
+                String lastPacketReceive = intent.getStringExtra("lastPacketReceive");
+                String byteIn = intent.getStringExtra("byteIn");
+                String byteOut = intent.getStringExtra("byteOut");
+
+                if (duration == null) duration = "00:00:00";
+                if (lastPacketReceive == null) lastPacketReceive = "0";
+                if (byteIn == null) byteIn = " ";
+                if (byteOut == null) byteOut = " ";
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+    };
+
+
+    /**
+     * Show toast message
+     * @param message: toast message
+     */
+    public void showToast(String message) {
+        Toast.makeText(getContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+
+
+    @Override
+    public void onResume() {
+        LocalBroadcastManager.getInstance(getActivity()).registerReceiver(broadcastReceiver, new IntentFilter("connectionState"));
+
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        LocalBroadcastManager.getInstance(getActivity()).unregisterReceiver(broadcastReceiver);
+        super.onPause();
+    }
+
+
+    @Override
+    public void onStop() {
+
+        super.onStop();
+    }
+
+
+    public boolean getInternetStatus() {
+        ConnectivityManager cm = (ConnectivityManager) getContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo nInfo = cm.getActiveNetworkInfo();
+
+        boolean isConnected = nInfo != null && nInfo.isConnectedOrConnecting();
+        return isConnected;
+    }
+}
